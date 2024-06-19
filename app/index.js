fbfa4b0d7c7f14bd3d58fded71ce1ae87aea8182
Miloš Lapiš <mlapis@mlc.cz>
2024-06-19 21:29:25 +0200

    wip(setup): No logger

--- app/index.js
+++ app/index.js
@@ -1,35 +1,6 @@
-const log = require('ssi-logger');
 const express = require('express');
 const app = express();
 const port = 3000;
-
-log.open({
-	console: {enable: false},
-	syslog: {enable: true, facility: 'LOG_LOCAL1'}
-});
-log.info('Hello the World');
-log.close();
-
-let i = 0;
-
-const emitLog = () => {
-	i = i + 1;
-	log.open({
-		console: {enable: false},
-		syslog: {enable: true, facility: 'LOG_LOCAL1'}
-	});
-
-	log.emerg('EMERG: Hello the World:', i);
-	log.alert('ALERT: Hello the World:', i);
-	log.crit('CRIT: Hello the World:', i);
-	log.error('ERROR: Hello the World:', i);
-	log.warn('WARN: Hello the World:', i);
-	log.notice('NOTICE: Hello the World:', i);
-	log.info('INFO: Hello the World:', i);
-	log.debug('DEBUG: Hello the World:', i);
-
-	log.close();
-}
 
 app.get('/', (req, res) => {
 	res.send(`... Hello!`);
@@ -38,5 +9,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 	console.log(`... listening on port ${port}`);
 	console.log('... application started');
-	setInterval(emitLog, 2000);
 });
